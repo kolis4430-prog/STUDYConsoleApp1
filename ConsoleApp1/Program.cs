83c5925b966e6e15d0ea7bbc161ade1ac363556f
kolis4430-prog <kolis4430@gmail.com>
2025-10-28 21:41:53 +0600

    Update Program.cs

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hEllo world");
+            Console.WriteLine("hEllo world !one more modification");
         }
     }
 }
